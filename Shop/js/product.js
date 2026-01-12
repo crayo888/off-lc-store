020250b5832dbe0d8dad88e1300eb8b0f1558dbd
crayo888 <senpaihamadaki888@gmail.com>
2026-01-12 00:38:57 -0300

    Ajusta estilos de hover e cor do rodapé, atualiza preço do produto na página da loja

--- Shop/js/product.js
+++ Shop/js/product.js
@@ -1,3 +1,10 @@
 //pega o id do produto na URL//
+const params = new URLSearchParams(window.location.search);
+const productId = parseInt(params.get("id"));
 
+
+//buscar o produto//
+const product = products.find(p => p.id === productId)
+
+//preenche a pagina//
 
